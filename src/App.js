176733830c19b9086d9affa4f75ce815449fca9f
Dora Gil <git@doragil.me>
2019-06-29 14:30:57 -0400

    Added API Call to ComponentDidMount w/Nate

--- src/App.js
+++ src/App.js
@@ -10,8 +10,63 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      
+      books : [],
+      query : '',
+      printType: 'all',
+      filter: 'partial',
+      apiKey : 'AIzaSyDGeGG17Dg3IbVNBSBebNHy8HZGt2vWsu0',
+      searchURL : 'https://www.googleapis.com/books/v1/volumes',
     }
+  }
+
+  componentDidMount() {
+    const params = {
+      q : this.state.query,
+      printType : this.state.printType,
+      filter: this.state.filter
+    }
+
+    function formatQueryParams(params) {
+      const queryItems = Object.keys(params)
+        .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+      return queryItems.join('&');
+    }
+    const queryString = formatQueryParams(params)
+    const url = this.state.searchURL + '?' + queryString;
+
+    const options = {
+      headers: new Headers({
+        "X-Api-Key": this.state.apiKey})
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later');
+        }
+        return res.json();
+      })
+      .then(data => {
+        const bookList = data.items.map(book => {
+          return {
+            id : book.id,
+            title : book.volumeInfo.title,
+            authors: book.volumeInfo.authors,
+            thumbnail : book.volumeInfo.imageLinks.smallThumbnail,
+            snippet : book.hasOwnProperty('searchInfo') ? book.searchInfo.textSnippet : '',
+            price : book.saleInfo.saleability === 'FOR_SALE' ? book.saleInfo.listPrice.amount : false,
+          }
+        })
+
+        this.setState({
+          books: bookList
+        })
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
   }
 
   render() {
